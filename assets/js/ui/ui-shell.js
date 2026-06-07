950e8ffedbb29ab3e24c58a02baa16f19e77534a
aleks-app <al822alex@gmail.com>
2026-06-07 18:49:58 +0300

    Add mobile map app-screen mode

--- assets/js/ui/ui-shell.js
+++ assets/js/ui/ui-shell.js
@@ -193,7 +193,7 @@ function updateHomeFloatButton() {
     return;
   }
 
-  if (document.body.getAttribute("data-app-chrome-mode") === "inner") {
+  if (["inner", "map"].includes(document.body.getAttribute("data-app-chrome-mode"))) {
     setHomeFloatButtonMode(btn, "");
     btn.classList.remove("show");
     return;
